21833358ef5fdb454ab9753cf951213244e4d464
Joyka <YaXymma@gmail.com>
2024-04-17 02:04:30 +0300

    Реализация DP3

---
+++ main-service/src/main/java/ru/practicum/ewm/dto/CommentDto.java
@@ -0,0 +1,21 @@
+package ru.practicum.ewm.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentDto {
+    private Long id;
+    private String text;
+    private Long authorId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime lastUpdatedOn;
+}
--- main-service/src/main/java/ru/practicum/ewm/dto/EventShortDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/EventShortDto.java
@@ -20,4 +20,5 @@ public class EventShortDto {
     private Boolean paid;
     private String title;
     private Long views;
+    private Long comments;
 }
